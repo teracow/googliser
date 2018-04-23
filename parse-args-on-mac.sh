ee41fe0b02eec6e3ef361f65b9a560ed7b9d15ad
teracow <teracow@gmail.com>
2018-04-24 07:08:23 +1000

    set env flag for getopt specifying full path to binary

--- parse-args-on-mac.sh
+++ parse-args-on-mac.sh
@@ -23,20 +23,21 @@
 ###############################################################################
 
 case "$OSTYPE" in
-	"darwin"*)
-		CMD_READLINK='greadlink'
-		CMD_HEAD='ghead'
-		CMD_SED='gsed'
-		CMD_DU='gdu'
-		CMD_LS='gls'
-		;;
-	*)
-		CMD_READLINK='readlink'
-		CMD_HEAD='head'
-		CMD_SED='sed'
-		CMD_DU='du'
-		CMD_LS='ls'
-		;;
+    "darwin"*)
+        CMD_READLINK='greadlink'
+        CMD_HEAD='ghead'
+        CMD_SED='gsed'
+        CMD_DU='gdu'
+        CMD_LS='gls'
+        export FLAGS_GETOPT_CMD="$(brew --prefix gnu-getopt)/bin/getopt"    # https://stackoverflow.com/a/47542834/6182835
+        ;;
+    *)
+        CMD_READLINK='readlink'
+        CMD_HEAD='head'
+        CMD_SED='sed'
+        CMD_DU='du'
+        CMD_LS='ls'
+        ;;
 esac
 
 user_parameters=$(getopt -o n:,p: -l number:,phrase: -n $($CMD_READLINK -f -- "$0") -- "$@")
@@ -44,81 +45,81 @@ user_parameters_result=$?
 user_parameters_raw="$@"
 
 Init()
-	{
+    {
 
-	local script_date='2018-04-24'
-	script_file='parse-args-on-mac.sh'
-	script_name="${script_file%.*}"
-	local script_details_plain="$script_file - $script_date PID:[$$]"
+    local script_date='2018-04-24'
+    script_file='parse-args-on-mac.sh'
+    script_name="${script_file%.*}"
+    local script_details_plain="$script_file - $script_date PID:[$$]"
 
-	# parameter defaults
-	images_required_default=25
+    # parameter defaults
+    images_required_default=25
 
-	# internals
-	local script_starttime=$(date)
-	script_startseconds=$(date +%s)
-	server='www.google.com'
-	useragent='Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-	target_path_created=false
-	show_help_only=false
-	exitcode=0
+    # internals
+    local script_starttime=$(date)
+    script_startseconds=$(date +%s)
+    server='www.google.com'
+    useragent='Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    target_path_created=false
+    show_help_only=false
+    exitcode=0
 
-	# user changable parameters
-	user_query=''
-	images_required=$images_required_default
-	debug=true
+    # user changable parameters
+    user_query=''
+    images_required=$images_required_default
+    debug=true
 
-	WhatAreMyOptions
+    WhatAreMyOptions
 
-	DebugThis '? $script_details' "$script_details_plain"
-	DebugThis '? $user_parameters_raw' "$user_parameters_raw"
+    DebugThis '? $script_details' "$script_details_plain"
+    DebugThis '? $user_parameters_raw' "$user_parameters_raw"
 
-	DebugThis '= environment' '*** parameters ***'
-	DebugThis '? $user_query' "$user_query"
-	DebugThis '? $images_required' "$images_required"
+    DebugThis '= environment' '*** parameters ***'
+    DebugThis '? $user_query' "$user_query"
+    DebugThis '? $images_required' "$images_required"
 
-	}
+    }
 
 WhatAreMyOptions()
-	{
+    {
 
-	[[ $user_parameters_result -ne 0 ]] && { echo; exitcode=2; return 1 ;}
-	[[ $user_parameters = ' --' ]] && { show_help_only=true; exitcode=2; return 1 ;}
+    [[ $user_parameters_result -ne 0 ]] && { echo; exitcode=2; return 1 ;}
+    [[ $user_parameters = ' --' ]] && { show_help_only=true; exitcode=2; return 1 ;}
 
-	eval set -- "$user_parameters"
+    eval set -- "$user_parameters"
 
-	while true; do
-		case "$1" in
-			-n|--number)
-				images_required="$2"
-				shift 2
-				;;
-			-p|--phrase)
-				user_query="$2"
-				shift 2
-				;;
-			--)
-				shift		# shift to next parameter in $1
-				break
-				;;
-			*)
-				break		# there are no more matching parameters
-				;;
-		esac
-	done
+    while true; do
+        case "$1" in
+            -n|--number)
+                images_required="$2"
+                shift 2
+                ;;
+            -p|--phrase)
+                user_query="$2"
+                shift 2
+                ;;
+            --)
+                shift       # shift to next parameter in $1
+                break
+                ;;
+            *)
+                break       # there are no more matching parameters
+                ;;
+        esac
+    done
 
-	return 0
+    return 0
 
-	}
+    }
 
 DebugThis()
-	{
+    {
 
-	# $1 = item
-	# $2 = value
+    # $1 = item
+    # $2 = value
 
-	echo "$1: '$2'"
+    echo "$1: '$2'"
 
-	}
+    }
 
 Init
